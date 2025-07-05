3ad311f9221e4a04a7630a0af33528dc4102c8ea
Dan <danaaboston@gmail.com>
2025-07-05 14:34:34 -0400

    electron app started

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import electron from 'vite-plugin-electron';
+import electronRenderer from 'vite-plugin-electron-renderer';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    electron({
+      entry: 'electron/main.ts',
+    }),
+    electronRenderer(),
+  ],
+});
